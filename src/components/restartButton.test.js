73ef3ff1fbcd84eff780388345aded2d1dc53aef
nk1433 <iamnk1433@gmail.com>
2021-08-18 11:03:21 +0000

    Refactored restartButton test.

--- src/components/restartButton.test.js
+++ src/components/restartButton.test.js
@@ -6,15 +6,19 @@ import Restart from './restartButton';
 describe('Restart', () => {
 	const { actions } = context;
 
+	test('Component render with appropriate styling.', () => {
+		const component = render(Restart()).getByRole('restart');
+
+		expect(component).toBeInTheDocument();
+		expect(component).toHaveClass('restart');
+	});
+
 	test('when clicked triggers the action, restart', () => {
 		jest.spyOn(actions, 'restart').mockImplementation();
-
 		const component = render(Restart()).getByRole('restart');
 
 		fireEvent.click(component);
 
-		expect(component).toBeInTheDocument();
-		expect(component).toHaveClass('restart');
 		expect(actions.restart).toHaveBeenCalled();
 	});
 });
